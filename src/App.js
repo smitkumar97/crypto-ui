d36c2f8d86cfaade49c5919b0653de77a70e146a
Smit Patel <smitkumar97@gmail.com>
2025-03-29 14:14:22 +0530

    removed price-dashboard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import './App.css';
-import PriceDashboard from './components/dashboard';
 import CryptoPriceChart from './components/chart';
 
 function App() {
   return (
     <div className="App">
-      {/* <PriceDashboard
-          price="63,179.71" 
-          changeAmount="2,161.42" 
-          changePercentage="3.54" 
-          isPositive={true} 
-      /> */}
       <CryptoPriceChart />
     </div>
   );
